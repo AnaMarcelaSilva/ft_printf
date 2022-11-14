10d5a7fe4005e4bf349769df62735548975c7996
Ana Marcela Rodrigues Silva <amarcela@c1r8p4.42sp.org.br>
2022-11-14 18:49:24 -0300

    final version

--- ft_isptr.c
+++ ft_isptr.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ispt.c                                          :+:      :+:    :+:   */
+/*   ft_isptr.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amarcela <amarcela@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 19:37:54 by amarcela          #+#    #+#             */
-/*   Updated: 2022/10/18 04:35:31 by amarcela         ###   ########.fr       */
+/*   Updated: 2022/10/18 05:23:55 by amarcela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_isptr(unsigned long nbr)
 
 	count = 0;
 	if (nbr == 0)
-		return (ft_putstr("0x0"));
+		return (ft_putstr("(nil)"));
 	count += ft_putstr("0x");
 	count += ft_putnbr(nbr, 'x');
 	return (count);
